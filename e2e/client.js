26db78aed49c9d20005cb2a23707ff3d65382013
Gregory Thompson <gregt87@gmail.com>
2015-10-17 18:44:52 -0400

    got certificate using request and express

--- e2e/client.js
+++ e2e/client.js
@@ -1,38 +1,56 @@
 'use strict';
 
 var tls = require('tls');
-var fs = require('fs');
 var pem = require('pem');
-var ursa = require('ursa');
+var request = require('request');
+
+pem.createPrivateKey(function (err, pemKeyObj) {
+  console.log("pemKeyObj = %j", pemKeyObj);
+
+  pem.createCertificate({days:1, selfSigned:true, serviceKey: pemKeyObj.key}, function(err, keys){
+    console.log("err = %j", err);
+    if(err) { console.log("err.message = %j", err.message);}
+
+    
+
+    console.log("cert = " + keys.certificate);
+    console.log("priv = " + keys.serviceKey);
+     
+    console.log("keys.certificate = %j", keys.certificate);
+
+    var options = {
+      // These are necessary only if using the client certificate authentication (so yeah, you need them)
+      key: keys.serviceKey,
+      cert: keys.certificate,
+      rejectUnauthorized: false,
+      url: "https://localhost:8081"
+     
+      // This is necessary only if the server uses the self-signed certificate
+      //ca: [ fs.readFileSync('server/server-certificate.pem') ]//HOW DO I IGNORE THIS
+    };
 
 
-pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
-  var options = {
-    // These are necessary only if using the client certificate authentication (so yeah, you need them)
-    key: keys.serviceKey,
-    cert: keys.certificate,
-    rejectUnauthorized: false
-   
-    // This is necessary only if the server uses the self-signed certificate
-    //ca: [ fs.readFileSync('server/server-certificate.pem') ]//HOW DO I IGNORE THIS
-  };
+    request.get(options, function (error, response, body) {
+      if(response.statusCode == 201 || (response.statusCode == 200)){
+        console.log("body = %j", body);
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    });
 
-  console.log("cert = " + keys.certificate);
-  console.log("priv = " + keys.serviceKey);
-   
-  console.log("keys.certificate = %j", keys.certificate);
-
-  var socketClearTextStream = tls.connect(8081, options, function() {
-    socketClearTextStream.write("hello my name is greg");
-    process.stdin.pipe(socketClearTextStream);
-    process.stdin.resume();
-  });
-  socketClearTextStream.setEncoding('utf8');
-  socketClearTextStream.on('data', function(data) {
-    // console.log("data");
-    // console.log(data);
-  });
-  socketClearTextStream.on('end', function() {
-    console.log("end")
+    // var socketClearTextStream = tls.connect(8081, options, function() {
+    //   socketClearTextStream.write("hello my name is greg");
+    //   process.stdin.pipe(socketClearTextStream);
+    //   process.stdin.resume();
+    // });
+    // socketClearTextStream.setEncoding('utf8');
+    // socketClearTextStream.on('data', function(data) {
+    //   // console.log("data");
+    //   // console.log(data);
+    // });
+    // socketClearTextStream.on('end', function() {
+    //   console.log("end")
+    // });
   });
 });
--- e2e/server.js
+++ e2e/server.js
@@ -1,14 +1,13 @@
 'use strict';
 
 var tls = require('tls');
-var net = require('net');
 var fs = require('fs');
 var sys = require('sys');
+var https = require('https');
 var cert_encoder = require('cert_encoder');
-
-
-var https = require('https'),
-    pem = require('pem');
+var express = require('express');
+var macattackExpress = require('macattack-express');
+var pem = require('pem');
 
 pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
   var options = {
@@ -27,48 +26,117 @@ pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
    
   };
 
-  // new tls.TLSSocket(socket, {});
-   
-  var server = tls.createServer(options, function(socketCleartextStream) {
+  var app = express();
 
-
-
-
-    console.log("started");
-    console.log("cert_encoder.convert(socketCleartextStream.getPeerCertificate().raw) = %j", cert_encoder.convert(socketCleartextStream.getPeerCertificate().raw));
-
-    console.log("socketCleartextStream.authorized = %j", socketCleartextStream.authorized);
-    console.log("socketCleartextStream.authorizationError = %j", socketCleartextStream.authorizationError);
+  app.route('/').get(function(req, res) {
     debugger;
 
-    //Show the certificate info as supplied by the client
-    // console.log(socketCleartextStream.getPeerCertificate());
+    console.log("req.connection.getPeerCertificate() = %j", req.connection.getPeerCertificate());
+    res.json({ index: "data" });
+  });
+
+  // Return Express server instance vial callback
+
+  console.log("keys.certificate = %j", keys.certificate);
+
+  pem.getPublicKey(keys.certificate, function (err, data){
+    console.log("data = %j", data);
+  });
+
+  var httpsApp = https.createServer(options, app);
+
+  httpsApp.listen(8081, '0.0.0.0');
+
+  // // new tls.TLSSocket(socket, {});
    
-    // console.log('server connected', socketCleartextStream.authorized ? 'authorized' : 'unauthorized');
-    // console.log("socketCleartextStream.getTLSTicket() = %j", socketCleartextStream.getTLSTicket());
-    // console.log("hello");
-    // console.log("socketCleartextStream = %j", socketCleartextStream);
-    socketCleartextStream.write("welcome!\n");
-    socketCleartextStream.setEncoding('utf8');
-    socketCleartextStream.pipe(socketCleartextStream);
+  // var server = tls.createServer(options, function(socketCleartextStream) {
+  //   console.log("started");
+  //   console.log("cert_encoder.convert(socketCleartextStream.getPeerCertificate().raw) = %j", cert_encoder.convert(socketCleartextStream.getPeerCertificate().raw));
 
-    socketCleartextStream.addListener("connect", function () {
-      sys.puts("Connection from " + socketCleartextStream.remoteAddress);
-    });
+  //   console.log("socketCleartextStream.authorized = %j", socketCleartextStream.authorized);
+  //   console.log("socketCleartextStream.authorizationError = %j", socketCleartextStream.authorizationError);
+  //   debugger;
 
-    socketCleartextStream.addListener("data", function (data) {
-      console.log("data = %j", data);
-      console.log("tempTlsSocket");
-    });
+  //   //Show the certificate info as supplied by the client
+  //   // console.log(socketCleartextStream.getPeerCertificate());
+   
+  //   // console.log('server connected', socketCleartextStream.authorized ? 'authorized' : 'unauthorized');
+  //   // console.log("socketCleartextStream.getTLSTicket() = %j", socketCleartextStream.getTLSTicket());
+  //   // console.log("hello");
+  //   // console.log("socketCleartextStream = %j", socketCleartextStream);
+  //   socketCleartextStream.write("welcome!\n");
+  //   socketCleartextStream.setEncoding('utf8');
+  //   socketCleartextStream.pipe(socketCleartextStream);
 
-    socketCleartextStream.addListener("close", function () {
-      //close the tunnel when the client finishes the connection.
-      server.close();
-    });
-  });
+  //   socketCleartextStream.addListener("connect", function () {
+  //     sys.puts("Connection from " + socketCleartextStream.remoteAddress);
+  //   });
 
-  server.listen(8081, function() {
-    console.log('server bound');
-  });
+  //   socketCleartextStream.addListener("data", function (data) {
+  //     console.log("data = %j", data);
+  //     console.log("tempTlsSocket");
+  //   });
 
+  //   socketCleartextStream.addListener("close", function () {
+  //     //close the tunnel when the client finishes the connection.
+  //     server.close();
+  //   });
+  // });
+
+  // server.listen(8081, function() {
+  //   console.log('server bound');
+  // });
 });
+
+
+// var cert = fs.readFileSync(certfile, "utf-8");
+
+// pem.getPublicKey(cert, function (err, data) {
+//   var caveatKey = crypto.createHash('md5').digest('hex');
+
+//   console.log("cert = %j", cert);
+
+//   function condenseCertificate(cert){
+//     return cert
+//       .replace("-----BEGIN CERTIFICATE-----", "")
+//       .replace("-----END CERTIFICATE-----", "")
+//       .replace(/\n/g, "");
+//   }
+
+//   var caveatMacaroon = publicKeyMacaroons.addPublicKey3rdPartyCaveat(serializedMacaroon, "For initializing client", caveatKey, "cert = " + condenseCertificate(cert), data.publicKey);
+
+//   console.log("client_macaroon=" + JSON.stringify(caveatMacaroon));
+
+//   //macattack_express
+//   app.use(macattack_express({secret: secretKey}));
+
+//   //end of macattack security
+
+//   app.disable('x-powered-by');
+
+//   // Globbing routing files
+//   config.getGlobbedFiles('./app/routes/**/*.js').forEach(function(routePath) {
+//     require(path.resolve(routePath))(app);
+//   });
+
+//   // Log SSL usage
+//   console.log('Securely using https protocol');
+
+//   // Create SSL key and certificate
+//   pem.createCertificate({days:1, selfSigned:true}, function(err, keys){
+    
+
+//     // Return Express server instance vial callback
+//     callback(https.createServer(options, app));
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
